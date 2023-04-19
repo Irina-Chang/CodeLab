1e45a499f8fa0975c68e0b6e88c23089b1b7bf1c
Irina Ferreira <irina.f@aluno.ifsp.edu.br>
2023-04-18 22:18:05 -0300

    reordenando pastas do repositorio

--- regescweb/src/main/java/br/com/codelab/regescweb/controllers/HelloController.java
+++ regescweb/src/main/java/br/com/codelab/regescweb/controllers/HelloController.java
@@ -1,12 +1,14 @@
 package br.com.codelab.regescweb.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
     @GetMapping("/hello")
-    public String hello() {
+    public String hello(HttpServletRequest request) {
+        request.setAttribute(name:"nome", o:"Polenguinho");
         return "hello"; //vai gerar a renderização do arquivo que esta dentro dos templates
     }
 }
