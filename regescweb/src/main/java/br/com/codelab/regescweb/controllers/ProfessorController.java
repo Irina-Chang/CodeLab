74f9aa65d7299ec0929034d583f5bf57519ee016
Irina Ferreira <irina.f@aluno.ifsp.edu.br>
2023-04-20 08:05:09 -0300

    Criando a conexao com banco de dados dinamico

--- regescweb/src/main/java/br/com/codelab/regescweb/controllers/ProfessorController.java
+++ regescweb/src/main/java/br/com/codelab/regescweb/controllers/ProfessorController.java
@@ -2,6 +2,8 @@ package br.com.codelab.regescweb.controllers;
 
 import br.com.codelab.regescweb.models.Professor;
 import br.com.codelab.regescweb.models.StatusProfessor;
+import br.com.codelab.regescweb.repositories.ProfessorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,23 +17,23 @@ import java.util.List;
 @Controller
 public class ProfessorController {
 
+    @Autowired
+    private ProfessorRepository professorRepository;
+
     @GetMapping("/professores")
     public ModelAndView index(){
-        Professor  garnet = new Professor( "Garnet", new BigDecimal(5000), StatusProfessor.ATIVO);
-        garnet.setId(1L);
-        Professor perola = new Professor("Perola", new BigDecimal(6000), StatusProfessor.APOSENTADO);
-        perola.setId(2L);
-        Professor ametista = new Professor("Ametista", new BigDecimal(8000), StatusProfessor.INATIVO);
-        ametista.setId(3L);
 
-        List<Professor> professores = Arrays.asList(garnet, perola, ametista);
-
-
+        List<Professor> professores = this.professorRepository.findAll();
         ModelAndView mv = new ModelAndView( "professores/index");
         mv.addObject("professores", professores);
-
 
         return mv;
 
     }
 }
+ /*Professor  garnet = new Professor( "Garnet", new BigDecimal(5000), StatusProfessor.ATIVO);
+        garnet.setId(1L);
+        Professor perola = new Professor("Perola", new BigDecimal(6000), StatusProfessor.APOSENTADO);
+        perola.setId(2L);
+        Professor ametista = new Professor("Ametista", new BigDecimal(8000), StatusProfessor.INATIVO);
+        ametista.setId(3L);*/
